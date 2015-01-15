43e85481e0b24dfdb37e25b2a43d578aa9b392c9
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 18:29:39 -0800

    Functional calculator with exemption handling.

--- exercise3.py
+++ exercise3.py
@@ -1,36 +1,48 @@
-# # No setup
-# repeat forever:
-#     read input
-#     tokenize input
-#     if the first token is 'q', quit
-#     otherwise decide which math function to call based on the tokens we read
 
+# import from py module in same folder
 from arithmetic import *
 
-def main():
-
+def input_and_parameter_check():
+    """Ask for user input, and confirms it has the correct number of parameters."""
     while True:
         user_input = raw_input(">> ")
         tokens = user_input.split(" ")
-        if tokens[0] =="q":
-            break
-        elif tokens[0] =="+":
-            print (add(int(tokens[1]), int(tokens[2])))
-        elif tokens[0] =="-":
-            print (subtract(int(tokens[1]), int(tokens[2])))
-        elif tokens[0] =="*":
-            print (multiply(int(tokens[1]), int(tokens[2])))
-        elif tokens[0] =="/":
-            print (divide(int(tokens[1]), int(tokens[2])))
-        elif tokens[0] =="square":
-            print (square(int(tokens[1])))
-        elif tokens[0] =="cube":
-            print (cube(int(tokens[1])))
-        elif tokens[0] =="pow":
-            print (power(int(tokens[1]), int(tokens[2])))
-        elif tokens[0] =="mod":
-            print (mod(int(tokens[1]), int(tokens[2])))
+        correct_num_items = {"+":3,"-":3,"*":3,"/":3,"square":2,"cube":2,"pow":3,"mod":3, "q":1}
+        if len(tokens) != correct_num_items[tokens[0]]:
+            print "You entered the wrong number of parameters. Enter Again."
         else:
+            return tokens
+
+def use_arithmetic_functions(to_calculate):
+    """Takes in the user input (parameters), calls the function, and provides the answer."""
+    if to_calculate[0] =="+":
+        print (add(float(to_calculate[1]), float(to_calculate[2])))
+    elif to_calculate[0] =="-":
+        print (subtract(float(to_calculate[1]), float(to_calculate[2])))
+    elif to_calculate[0] =="*":
+        print (multiply(float(tto_calculate[1]), float(to_calculate[2])))
+    elif to_calculate[0] =="/":
+        print (divide(float(to_calculate[1]), float(to_calculate[2])))
+    elif to_calculate[0] =="square":
+        print (square(float(to_calculate[1])))
+    elif to_calculate[0] =="cube":
+        print (cube(float(to_calculate[1])))
+    elif to_calculate[0] =="pow":
+        print (power(float(to_calculate[1]), float(to_calculate[2])))
+    elif to_calculate[0] =="mod":
+        print (mod(float(to_calculate[1]), float(to_calculate[2])))
+
+def main():
+    """calls to the function for user to enter input, allows for quitting, 
+    nd recognizes invalid input (not a list of parameters)"""
+    while True:
+        try:
+            input_for_arthimetic_funct = input_and_parameter_check()
+            if input_for_arthimetic_funct[0] =="q":
+                break
+            else:
+                use_arithmetic_functions(input_for_arthimetic_funct)
+        except:
             print "enter a valid input"
 
 if __name__ == "__main__":
